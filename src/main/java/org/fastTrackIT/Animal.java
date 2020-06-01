17c380b8264327d71728d2fba7ec392c168a8469
Florin Vlas <vlaspetruflorin@gmail.com>
2020-06-01 14:24:29 +0300

    added animal class

---
+++ src/main/java/org/fastTrackIT/Animal.java
@@ -0,0 +1,13 @@
+package org.fastTrackIT;
+
+public class Animal {
+    String name;
+    double age;
+    double health;
+    double hunger;
+    double mood;
+    String royalCanin;
+    String running;
+    boolean dead;
+
+}
